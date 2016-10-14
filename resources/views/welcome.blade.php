f42ae41847050ffb09a72f992e9a80d96e04a83c
Cattreeen <speak2cathie@yahoo.com>
2016-10-14 09:30:57 +0100

    Second commit- Modified the Welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,16 +76,26 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome To My First Page
+                </div>
+                <div class="container">
+                <form action="{{url('/'')}}" method="post">
+                        
+                    <div><label for="email">Email:</label><br><input type="text"></div>
+                    <div><label for="password">Password:</label><br><input type="password"></div>
+                    <br><br>
+                    <div><input type="submit" value="Login"></div>
+
+                    </form>
                 </div>
 
-                <div class="links">
+                <!-- <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </body>
